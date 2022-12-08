a7b071d8d7b58c39b08f73273290ccdc5013a068
Eduarda Neumann <eduardafneumann@gmail.com>
2022-12-08 09:42:01 -0300

    gerar casos de teste

--- Projeto 2/set.h
+++ Projeto 2/set.h
@@ -1,18 +1,22 @@
 #ifndef SET_H
 #define SET_H
 
-#include "avl.h"
 #include "item.h"
 
 typedef struct set_ SET;
 
-SET *set_criar(void);
-bool set_pertence(SET *A, int elemento);
-bool set_inserir(SET *s, int elemento);
-bool set_remover(SET *s, int elemento);
-void set_apagar(SET **s);
-void set_imprimir(SET *s);
-SET *set_uniao(SET *A, SET *B);
-SET *set_interseccao(SET *A, SET *B);
+// operações básicas
+
+SET *set_criar();
+void set_apagar(SET **set);
+void set_inserir(SET *set, int chave);
+void set_remover(SET *set, int chave);
+void set_imprimir(SET *set);
+
+// operações específicas
+
+bool set_pertence(SET* set, int chave);
+SET *set_interseccao(SET *set1, SET *set2);
+SET *set_uniao(SET *set1, SET *set2);
 
 #endif
